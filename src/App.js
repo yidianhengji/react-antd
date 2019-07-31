0d8463a59d3a7dd2c3f4dc8e144c4340c6b27bef
yidianhengji <493871850@qq.com>
2019-07-31 17:43:52 +0800

    router

--- src/App.js
+++ src/App.js
@@ -1,29 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Home from './views/home'
-import Nes from './views/nes'
 import Login from './views/login/login'
+import home from './views/home/index'
 function BasicExample() {
     return (
-        <Login></Login>
-        /*<Router>
-            <div>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/topics">Topics</Link>
-                    </li>
-                    <li>
-                        <Link to="/home">home</Link>
-                    </li>
-                </ul>
-                <Route exact path="/" component={Home} />
-                <Route path="/topics" component={Nes} />
-                <Route path="/home" component={Home} />
-            </div>
-        </Router>*/
+        <Router>
+            <Route exact path="/" component={Login} />
+            <Route path="/home" component={home} />
+        </Router>
     );
 }
 
